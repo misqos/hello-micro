0fc3201d283d6d136e927e62a829c950b74764b9
Lukasz Petka <lukasz_petka@interia.pl>
2017-12-14 00:25:13 +0100

    Added application to consul, mongo still not working

--- greeting/src/main/java/pl/edu/agh/iosr/hello/HelloApplication.java
+++ greeting/src/main/java/pl/edu/agh/iosr/hello/HelloApplication.java
@@ -3,6 +3,8 @@ package pl.edu.agh.iosr.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
