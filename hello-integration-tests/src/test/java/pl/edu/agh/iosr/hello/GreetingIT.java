4bcaf93d95638d8160f6126ce98ffdba3f480736
Michał Furdyna <mr.miskov@gmail.com>
2018-01-30 14:00:32 +0100

    integration tests

---
+++ hello-integration-tests/src/test/java/pl/edu/agh/iosr/hello/GreetingIT.java
@@ -0,0 +1,40 @@
+package pl.edu.agh.iosr.hello;
+
+import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.connection.waiting.HealthChecks;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import org.apache.http.client.utils.URIBuilder;
+import org.awaitility.Duration;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.awaitility.Awaitility.given;
+
+public class GreetingIT {
+    @ClassRule
+    public static DockerComposeRule docker = DockerComposeRule.builder()
+            .file("../consul.yml")
+            .waitingForService("mongodb", HealthChecks.toHaveAllPortsOpen())
+            .waitingForService("consul", HealthChecks.toHaveAllPortsOpen())
+            .waitingForService("greeting", HealthChecks.toHaveAllPortsOpen())
+            .waitingForService("visits", HealthChecks.toHaveAllPortsOpen())
+            .build();
+
+    @Test
+    public void simpleTest() {
+        given().ignoreExceptions().await().atMost(Duration.ONE_MINUTE).untilAsserted(() ->
+                assertThat(new OkHttpClient.Builder().build().newCall(
+                        new Request.Builder()
+                                .url(HttpUrl.parse(
+                                        new URIBuilder()
+                                                .setScheme("http")
+                                                .setHost("127.0.0.1")
+                                                .setPort(8080).toString()
+                                        ).newBuilder().addPathSegments("greeting").build()
+                                ).get().build()
+                ).execute().code()).isEqualTo(200));
+    }
+}
